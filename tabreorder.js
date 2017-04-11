0daa221480426f8c1c3462be4491023c4d1e0131
Daniel <daniel.seehausen@gmail.com>
2017-04-11 15:28:55 -0400

    Version 0.1.0 being published now

--- tabreorder.js
+++ tabreorder.js
@@ -2,6 +2,7 @@ document.addEventListener("keydown", function(e) {
   //only pass if the right modifiers are down and it is a valid hotkey
   if ((e.shiftKey && e.metaKey) && (e.keyCode == 80 || e.keyCode == 220)) {
     chrome.extension.sendMessage({keyCode: e.keyCode})
+    console.log("Sent mdg")
     e.stopPropagation()
     e.preventDefault()
   }
